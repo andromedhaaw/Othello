ee4a942b6d0cdaeb7e57172999198b5602e1e409
andromedhaaw <andromedha@students.undip.ac.id>
2025-03-18 09:27:15 +0700

    update

--- MoveHistory.cs
+++ MoveHistory.cs
@@ -33,31 +33,6 @@ namespace OthelloGame
             _history.Add((boardCopy, currentPlayer));
         }
 
-        public bool CanUndo()
-        {
-            return _history.Count > 0;
-        }
-
-        public void Undo(Board board, ref Player currentPlayer)
-        {
-            if (!CanUndo()) return;
-
-            var lastState = _history[_history.Count - 1];
-            Piece?[,] originalBoard = board.GetBoard();
-            Piece?[,] savedBoard = lastState.boardState;
-            int size = originalBoard.GetLength(0);
-
-            for (int i = 0; i < size; i++)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    originalBoard[i, j] = savedBoard[i, j];
-                }
-            }
-
-            currentPlayer = lastState.player;
-            _history.RemoveAt(_history.Count - 1);
-            Console.WriteLine("Last move undone.");
-        }
+        
     }
 }
